0e68934b14113c50b73d41b9ba30b450bc30f5c0
Kevin Printz <kevinprintz@microsoft.com>
2023-03-19 20:29:33 -0400

    fix

--- terraform/vm/resource.tf
+++ terraform/vm/resource.tf
@@ -59,15 +59,3 @@ resource "azurerm_linux_virtual_machine" "packer" {
     version   = "latest"
   }
 }
-
-# Get the IP address
-output "vm_ip_addr" {
-  value = azurerm_public_ip.pip.ip_address
-  sensitive = false
-}
-
-# Get the VM password
-output "vm_secret" {
-  value = data.azurerm_key_vault_secret.secret.value
-  sensitive = true
-}
